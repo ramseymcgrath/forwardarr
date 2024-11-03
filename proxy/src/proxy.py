852d7478e925937a5ae395008131d700e21903c7
Ramsey McGrath <2006510+ramseymcgrath@users.noreply.github.com>
2024-11-03 01:01:55 -0400

    Allow configuring the user agent in requests (#15)
    Signed-off-by: ramseymcgrath <ramseymcgrath@gmail.com>

--- proxy/src/proxy.py
+++ proxy/src/proxy.py
@@ -231,12 +231,18 @@ def handle_request(indexer_name, request_type="api"):
         # Otherwise, add the indexer key to the query
         else:
             indexer_query.update({user_id_param: indexer_key})
-
+        # Add user agent to request headers if specified
+        try:
+            request_user_agent = indexer_info["user_agent"]
+        # Otherwise, use the requestor user agent if available
+        except:
+            request_user_agent = request.headers.get("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.6 Safari/605.1.15")
         # Prepare headers
         headers = {
             "Host": urlparse(usenet_server_url).netloc,
             "Accept-Encoding": "gzip, deflate",
-            "User-Agent": request.headers.get("User-Agent", "ForwardArr Proxy"),
+            "User-Agent": request_user_agent,
+            "Connection": "keep-alive",
         }
 
         app.logger.debug(f"Request headers: {headers}")
@@ -358,7 +364,10 @@ def handle_request(indexer_name, request_type="api"):
 
             app.logger.debug(f"Response headers to client: {response_headers}")
             app.logger.debug(f"Response status code: {response.status_code}")
-
+            statsd.increment(
+                "forwardarr.grab_request_response.count",
+                tags=[f"grab_request:true", f"indexer:{indexer_name}"],
+            )
             return Response(generate(), response.status_code, response_headers)
         ## Otherwise, we can just return the response
         else:
@@ -368,7 +377,10 @@ def handle_request(indexer_name, request_type="api"):
                 for (name, value) in response.raw.headers.items()
                 if name.lower() not in excluded_headers
             ]
-
+            statsd.increment(
+                "forwardarr.other_request_response.count",
+                tags=[f"grab_request:false", f"indexer:{indexer_name}"],
+            )
             app.logger.debug(f"Response headers to client: {response_headers}")
             app.logger.debug(f"Response status code: {response.status_code}")
 
